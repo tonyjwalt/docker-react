b4e417f862a9b6c955857b5c3985fead0d63602b
tony walt <tony.walt@charter.com>
2020-12-06 16:05:05 -0700

    chore(app.js) Change message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Feature branch change
         </a>
       </header>
     </div>
